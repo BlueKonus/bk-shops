637e7dc216dbf31ae0506e0d89c3760816dbe27b
BlueKonus <83145490+BlueKonus@users.noreply.github.com>
2021-08-24 17:49:16 +0200

    Create config.lua

---
+++ config.lua
@@ -0,0 +1,41 @@
+Config = {}
+
+AMOUNT_TO_BUY = 1
+
+Config.DrawDistance = 100
+
+Config.Shops = {    
+    {name = 'water', label = 'Voda', price = 60},
+    {name = 'bread', label = 'Chleba', price = 60}, 
+    {name = 'phone', label = 'Mobil', price = 60},   
+}
+
+-- Shop 3D Text
+Config.Pos = {
+    a = {
+        Pos = vector3(-47.72, -1757.19, 29.30),
+    },
+    b = {
+        Pos = vector3(25.74, -1347.13, 29.5),
+    },
+    c = {
+        Pos = vector3(-707.39, -913.69, 19.22),
+    },
+    d = {
+        Pos = vector3(1135.6, -982.47, 46.32),
+    },
+    e = {
+        Pos = vector3(1163.6, -323.98, 69.21),
+    },
+    f = {
+        Pos = vector3(373.83, 326.06, 103.47),
+    },
+    g = {
+        Pos = vector3(-1487.6, -378.67, 40.16),
+    },
+    h = {
+        Pos = vector3(-1222.43, -906.92, 12.23),
+    }
+}
+
+
