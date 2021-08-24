45852718ddcd7f1f779ffb9b38d52610bbe136bd
BlueKonus <83145490+BlueKonus@users.noreply.github.com>
2021-08-24 17:48:47 +0200

    Create fxmanifest.lua

---
+++ fxmanifest.lua
@@ -0,0 +1,14 @@
+fx_version 'adamant'
+game 'gta5'
+autor 'BlueKonus'
+Version '0.1'
+
+client_scripts {
+    'config.lua',
+    'client/*.lua'
+}
+
+server_scripts {
+    'config.lua',
+    'server/*.lua'
+}
